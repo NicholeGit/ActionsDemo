eb431d0e569dbe9112bc0d4a5b03d1ab00ccdabc
Nichole <123576128@qq.com>
2022-06-17 14:32:40 +0800

    go.yml 整理

--- main.go
+++ main.go
@@ -2,6 +2,15 @@ package main
 
 import "log"
 
+var (
+	appVersion   string
+	appRevision  string
+	appBuildDate string
+)
+
 func main() {
 	log.Println("start ...")
+	log.Println("appVersion:", appVersion)
+	log.Println("appRevision:", appRevision)
+	log.Println("appBuildDate:", appBuildDate)
 }
